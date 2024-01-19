4965d10764a9a6d25b98b6509de1cb6135b195fc
Etienne LEGRAND <etienne.legrand3@ecoles-epsi.net>
2024-01-19 21:27:52 +0100

    Ajout de commentaires

--- producteur/producteur_congestion.js
+++ producteur/producteur_congestion.js
@@ -1,16 +1,14 @@
+// Importation des modules nécessaires
 import axios from "axios";
 import { Kafka, Partitioners } from "kafkajs";
 import moment from "moment";
 
-const kafka = new Kafka({
-  clientId: "producteur_congestion",
-  brokers: ["localhost:9092"],
-});
+// Configuration de l'objet Kafka pour la connexion au broker Kafka
+const kafka = new Kafka({clientId: "producteur_congestion", brokers: ["localhost:9092"]});
 const topic = "indice_congestion_moyen";
-const producer = kafka.producer({
-  createPartitioner: Partitioners.DefaultPartitioner,
-});
+const producer = kafka.producer({createPartitioner: Partitioners.DefaultPartitioner});
 
+// Calcul de l'indice de congestion en fonction du statut de trafic d'un tronçon
 const calculateCongestionIndex = (troncon) => {
   let congestionIndex = 0;
 
@@ -29,6 +27,7 @@ const calculateCongestionIndex = (troncon) => {
   return congestionIndex;
 };
 
+// Envoie un message Kafka en récupérant les données de trafic à partir de l'API
 const produceMessage = async (datetime) => {
   try {
     console.log(`Récupération des données pour le ${datetime}`);
@@ -39,14 +38,14 @@ const produceMessage = async (datetime) => {
       const key = troncon.predefinedLocationReference;
       const congestionIndex = calculateCongestionIndex(troncon);
 
-      console.log(`Tronçon: ${key}    Taux de congestion: ${congestionIndex}    Date: ${datetime}`);
-      
+      console.log(`Tronçon: ${key}, Taux de congestion: ${congestionIndex}, Date: ${datetime}`);
+
       await producer.send({
         topic,
         messages: [{
-          key,
-          value: JSON.stringify({ congestionIndex, datetime }),
-        }],
+            key,
+            value: JSON.stringify({ congestionIndex, datetime }),
+          }],
       });
     });
   } catch (error) {
@@ -54,14 +53,16 @@ const produceMessage = async (datetime) => {
   }
 };
 
+// Fonction principale pour exécuter le processus d'envoi de messages à Kafka
+// en parcourant les données de trafic entre deux dates
 const run = async () => {
   await producer.connect();
-  
+
   const startTime = moment("2024-01-17T14:09:00+01:00");
   const endTime = moment("2024-01-17T14:50:00+01:00");
   let currentTime = moment(startTime);
 
-  // Itérer sur chaque minute entre startTime et endTime pour parcourir les données
+  // Itération sur chaque minute entre startTime et endTime pour parcourir les données
   // et envoyer un message à Kafka
   while (currentTime <= endTime) {
     await produceMessage(currentTime.format());
--- producteur/producteur_vitesse.js
+++ producteur/producteur_vitesse.js
@@ -1,34 +1,35 @@
+// Importation des modules nécessaires
 import axios from "axios";
 import { Kafka, Partitioners } from "kafkajs";
 import moment from "moment";
 
-const kafka = new Kafka({
-  clientId: "producteur_vitesse",
-  brokers: ["localhost:9092"],
-});
+// Configuration de l'objet Kafka pour la connexion au broker Kafka
+const kafka = new Kafka({clientId: "producteur_vitesse", brokers: ["localhost:9092"]});
 const topic = "vitesse_moyenne_localisation";
-const producer = kafka.producer({
-  createPartitioner: Partitioners.DefaultPartitioner,
-});
+const producer = kafka.producer({createPartitioner: Partitioners.DefaultPartitioner});
 
+// Envoie un message Kafka en récupérant les données de trafic à partir de l'API
 const produceMessage = async (datetime) => {
   try {
+    // Récupération des données de trafic à partir de l'API
     console.log(`Récupération des données pour le ${datetime}`);
     const response = await axios.get(`http://localhost:3000/api/trafic/${datetime}`);
     const trafficData = response.data;
 
+    // Parcours des données de trafic et envoi des messages à Kafka pour chaque tronçon
     trafficData.forEach(async (troncon) => {
       const key = troncon.predefinedLocationReference;
       const averageVehicleSpeed = troncon.averageVehicleSpeed;
 
       console.log(`Tronçon: ${key}    Vitesse moyenne: ${averageVehicleSpeed} km/h    Date: ${datetime}`);
-      
+
+      // Envoi du message à Kafka
       await producer.send({
         topic,
         messages: [{
-          key,
-          value: JSON.stringify({ averageVehicleSpeed, datetime }),
-        }],
+            key,
+            value: JSON.stringify({ averageVehicleSpeed, datetime }),
+          }],
       });
     });
   } catch (error) {
@@ -36,6 +37,7 @@ const produceMessage = async (datetime) => {
   }
 };
 
+// Fonction principale pour exécuter le processus d'envoi de messages à Kafka
 const run = async () => {
   await producer.connect();
 
@@ -43,7 +45,7 @@ const run = async () => {
   const endTime = moment("2024-01-17T14:50:00+01:00");
   let currentTime = moment(startTime);
 
-  // Itérer sur chaque minute entre startTime et endTime pour parcourir les données
+  // Itération sur chaque minute entre startTime et endTime pour parcourir les données
   // et envoyer un message à Kafka
   while (currentTime <= endTime) {
     await produceMessage(currentTime.format());
